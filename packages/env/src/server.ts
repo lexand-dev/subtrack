5573d831c46268fee99ed728b07e620d735dd5d0
alexander flores <alexfloresrayme@gmail.com>
2026-03-09 16:18:11 -0500

    fix: resolve native build errors

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -13,7 +13,7 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
   },
   client: {},
-  clientPrefix: "PUBLIC_",
+  clientPrefix: "",
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
 });
